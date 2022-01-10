00026f0c1afab7ed23c468f725096cb46b9a37ea
Yasiratiextendlabs <mianyasir@iextendlabs.com>
2022-01-11 00:50:49 +0500

    Text "Proudly Canadian - Designed in Edmonton, AB, Canada." on footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -53,6 +53,13 @@
                     </div>
                 </div>
             </div>
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-lg-12 col-xs-12">
+                    <div class="cps-widget custom-menu-widget">
+                        <p class="text-center">Proudly Canadian - Designed in Edmonton, AB, Canada.</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
