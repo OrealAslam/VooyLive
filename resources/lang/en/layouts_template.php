1880e8be58995ebb336abc06c6084bbcc17f433b
Yasiratiextendlabs <mianyasir@iextendlabs.com>
2022-02-01 02:05:05 +0500

    Cookie policy notification text update typo corrected

--- resources/lang/en/layouts_template.php
+++ resources/lang/en/layouts_template.php
@@ -1,7 +1,7 @@
 <?php
 return[
     'toggleNavigation'=>'Toggle navigation',
-    'para1'=>'This website uses cookies to improve your experience when browsing our website. By clicking on the "Accept Cookies" button or continuing to use this site, you are agreeing to their use. Learn More about our use of cookies and how to restrict usage in our',
+    'para1'=>'This website uses cookies to improve your experience when browsing our website. By clicking on the "Accept Cookies" button or continuing to use this site, you are agreeing to our use. Learn More about our use of cookies and how to restrict usage in our',
     'cookiePolicy'=>'Cookie Policy.',
     'acceptCookies'=>'Accept Cookies',
 ];
--- resources/lang/fr/layouts_template.php
+++ resources/lang/fr/layouts_template.php
@@ -1,7 +1,7 @@
 <?php
 return[
     'toggleNavigation'=>'Basculer la navigation',
-    'para1'=>'Ce site utilise des cookies pour améliorer votre expérience lors de la navigation sur notre site. En cliquant sur le bouton "Accepter les cookies" ou en continuant à utiliser ce site, vous acceptez leur utilisation. En savoir plus sur notre utilisation des cookies et comment restreindre lutilisation dans notre',
+    'para1'=>'Ce site utilise des cookies pour améliorer votre expérience lors de la navigation sur notre site. En cliquant sur le bouton "Accepter les cookies" ou en continuant à utiliser ce site, vous acceptez notre utilisation. En savoir plus sur notre utilisation des cookies et comment restreindre lutilisation dans notre',
     'cookiePolicy'=>'Politique en matière de cookies.',
     'acceptCookies'=>'Accepter les cookies',
 ];
