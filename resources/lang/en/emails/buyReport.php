d713e38093796df4ddd9ba532a62b9bd20bd904e
Yasiratiextendlabs <mianyasir@iextendlabs.com>
2022-02-08 21:02:05 +0500

    Minor fix

--- resources/lang/en/emails/buyReport.php
+++ resources/lang/en/emails/buyReport.php
@@ -3,6 +3,7 @@ return[
     'hey'=>'Hey',
     "para1"=>"Below is a summary of your recent purchase. You can download your purchased item at your 'My CFS reports' page and view your invoice(s) on your statement here.",
     'name'=>'Name',
+    'address'=>'Address',
     'amount'=>'Amount',
     'date'=>'Date',
     'action'=>'Action',
