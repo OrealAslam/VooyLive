33d3d625157194a6685539a2967ef271992d8eea
Yasiratiextendlabs <mianyasir@iextendlabs.com>
2022-02-10 04:16:25 +0500

    Mail config service provider boot method update

--- app/Providers/MailConfigServiceProvider.php
+++ app/Providers/MailConfigServiceProvider.php
@@ -16,22 +16,7 @@ class MailConfigServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $emailServices = SmtpSetting::first();
-        if ($emailServices) {
-            $config = array(
-                'driver'     => $emailServices->mail_driver,
-                'host'       => $emailServices->mail_host,
-                'port'       => $emailServices->mail_port,
-                'username'   => $emailServices->mail_username,
-                'password'   => $emailServices->mail_password,
-                'encryption' => $emailServices->mail_encryption,
-                'from'       => array('address' => $emailServices->mail_from_address, 'name' => $emailServices->mail_from_name),
-                'sendmail'   => '/usr/sbin/sendmail -bs',
-                'pretend'    => false,
-            );
-
-            Config::set('mail', $config);
-        }
+        //
     }
 
     /**
